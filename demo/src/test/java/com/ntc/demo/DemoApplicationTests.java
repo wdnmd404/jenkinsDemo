bb3f1200c9411ede75ba562025f9a9200e0fd469
wdnmd404 <13651370867@163.com>
2022-08-29 10:33:38 +0800

    1111

---
+++ demo/src/test/java/com/ntc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
